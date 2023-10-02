86537fb9a15d38ad4b8e39ae54895b7b1c88f60a
Adrián Jáimez <adroloja@gmail.com>
2023-10-02 11:30:59 +0200

    Function ViewPassengerTrain done

--- src/main/java/com/adrianj/trainproject/domain/repositories/TicketRepository.java
+++ src/main/java/com/adrianj/trainproject/domain/repositories/TicketRepository.java
@@ -2,13 +2,18 @@ package com.adrianj.trainproject.domain.repositories;
 
 import com.adrianj.trainproject.domain.entities.Passenger;
 import com.adrianj.trainproject.domain.entities.Ticket;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends CrudRepository<Ticket, Long> {
 
     Optional<Ticket> findByPassenger(Passenger passenger);
+
+    @Query("select u from Ticket u where u.train.number = ?1 and u.day = ?2")
+    Optional<List<Ticket>>  getListPassenger(String trainNUmber, String date);
 }
--- src/main/java/com/adrianj/trainproject/usecase/employees/ViewPassengerTrain.java
+++ src/main/java/com/adrianj/trainproject/usecase/employees/ViewPassengerTrain.java
@@ -1,28 +1,89 @@
 package com.adrianj.trainproject.usecase.employees;
 
 import com.adrianj.trainproject.domain.entities.Passenger;
+import com.adrianj.trainproject.domain.entities.Ticket;
+import com.adrianj.trainproject.domain.entities.Train;
 import com.adrianj.trainproject.domain.repositories.ScheduleRepository;
+import com.adrianj.trainproject.domain.repositories.TicketRepository;
 import com.adrianj.trainproject.domain.repositories.TrainRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ViewPassengerTrain {
 
     private TrainRepository trainRepository;
     private ScheduleRepository scheduleRepository;
+    private TicketRepository ticketRepository;
 
     @Autowired
-    public ViewPassengerTrain(TrainRepository trainRepository, ScheduleRepository scheduleRepository){
+    public ViewPassengerTrain(TrainRepository trainRepository, ScheduleRepository scheduleRepository, TicketRepository ticketRepository){
 
         this.trainRepository = trainRepository;
         this.scheduleRepository = scheduleRepository;
+        this.ticketRepository = ticketRepository;
     }
 
     @PostMapping("/getPassengerTrain")
-    public ResponseEntity<List<Passenger>> getPassangerTrain()
+    public ResponseEntity<List<Passenger>> getPassangerTrain(@RequestBody RequestGetPassengerTrain requestGetPassengerTrain){
+
+        Optional<List<Ticket>> optionalTicketListticketList = ticketRepository.getListPassenger(requestGetPassengerTrain.getTrainNumber(), requestGetPassengerTrain.getDay());
+
+        List<Passenger> passengerList;
+
+        if(optionalTicketListticketList.isPresent()){
+
+            List<Ticket> ticketList = optionalTicketListticketList.get();
+
+            passengerList = new ArrayList<>();
+
+            ticketList.forEach( ticket -> {
+
+                passengerList.add(ticket.getPassenger());
+            });
+
+            return ResponseEntity.ok(passengerList);
+
+        }else {
+
+            return ResponseEntity.status(500).build();
+        }
+    }
+}
+
+class RequestGetPassengerTrain {
+
+    private String trainNumber;
+    private String day;
+
+    public RequestGetPassengerTrain(String trainNumber, String day) {
+        this.trainNumber = trainNumber;
+        this.day = day;
+    }
+
+    public RequestGetPassengerTrain() {
+    }
+
+    public String getTrainNumber() {
+        return trainNumber;
+    }
+
+    public void setTrainNumber(String trainNumber) {
+        this.trainNumber = trainNumber;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
 }
